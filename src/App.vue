e82936781b4763fff873fccbff88a1209947277e
MUHAMMAD-WARIS\waris <wariszargardev@gmail.com>
2023-04-30 10:55:26 +0500

    Pagination code

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import nextPrevousPagination from "@/components/NextPrevousPagination.vue";
+import Properpagination from "@/components/Properpagination.vue";
+</script>
+
+<template>
+<!--  <nextPrevousPagination />-->
+  <properpagination />
+</template>
+
